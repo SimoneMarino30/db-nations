035921d6be4cd5c00616f5fb12fd4cf500f81808
Simone Marino <simo.marino@outlook.it>
2023-06-16 16:43:10 +0200

    Milestone 3 done

--- src/org/lessons/java/nations/Main.java
+++ src/org/lessons/java/nations/Main.java
@@ -1,6 +1,7 @@
 package org.lessons.java.nations;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,11 +11,23 @@ public class Main {
 
         try (Connection con = DriverManager.getConnection(url, user, password)) {
             System.out.println(con.getCatalog());
-            String sql = "SELECT c.name as country, c.country_id, r.name as region, cont.name as continent FROM countries c JOIN regions r ON c.region_id = r.region_id JOIN continents cont ON r.continent_id = cont.continent_id ORDER BY c.name;";
+            //String sql = "SELECT c.name as country, c.country_id, r.name as region, cont.name as continent FROM countries c JOIN regions r ON c.region_id = r.region_id JOIN continents cont ON r.continent_id = cont.continent_id ORDER BY c.name;";
+            //System.out.println("");
+            //System.out.println("Query: " + sql);
+            //System.out.println("");
+// ------------------------------------------------------------------------------------------
+            Scanner scan = new Scanner(System.in);
+            System.out.println("What would you like to search for?");
+            String wantedString = scan.nextLine();
+            //scan.close();
+            String safeSql = "SELECT c.name as country, c.country_id, r.name as region, cont.name as continent FROM countries c JOIN regions r ON c.region_id = r.region_id JOIN continents cont ON r.continent_id = cont.continent_id WHERE c.name LIKE ? ORDER BY c.name;";
             System.out.println("");
-            System.out.println("Query: " + sql);
+            System.out.println("Query: " + safeSql);
             System.out.println("");
-            try(PreparedStatement ps = con.prepareStatement(sql)) {
+            try(PreparedStatement ps = con.prepareStatement(safeSql)) {
+
+                ps.setString(1, "%" + wantedString +"%");
+
                 try(ResultSet rs = ps.executeQuery()) {
                     while(rs.next()) {
                         String country = rs.getString(1);
@@ -23,11 +36,12 @@ public class Main {
                         String continent = rs.getString(4);
                         System.out.println("Country: " + country + " | " + "Country-id: " + countryId + " | " + "Region: " + region + " | " + "Continent: " + continent);
                         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");
-                    }
+                }
                 }
             }
         } catch (SQLException e) {
             System.out.println("Sorry, unable to connect");
+            e.printStackTrace();
         }
     }
 }
