0193e81c142188570a64cad161b7ba0b3de75369
Simone Marino <simo.marino@outlook.it>
2023-06-18 21:56:46 +0200

    Added ID Validation

--- src/org/lessons/java/nations/Main.java
+++ src/org/lessons/java/nations/Main.java
@@ -11,13 +11,15 @@ public class Main {
 
         try (Connection con = DriverManager.getConnection(url, user, password)) {
             System.out.println(con.getCatalog());
+
+            /* MILESTONE 2 */
             //String sql = "SELECT c.name as country, c.country_id, r.name as region, cont.name as continent FROM countries c JOIN regions r ON c.region_id = r.region_id JOIN continents cont ON r.continent_id = cont.continent_id ORDER BY c.name;";
             //System.out.println("");
             //System.out.println("Query: " + sql);
             //System.out.println("");
-// ------------------------------------------------------------------------------------------
+// --------------------------------MILESTONE 3-----------------------------------------------
             Scanner scan = new Scanner(System.in);
-            System.out.println("What country would you like to search for?");
+            System.out.println("What country would you like to look for?");
             String wantedString = scan.nextLine();
             //scan.close();
             String safeSql = "SELECT c.name as country, c.country_id, r.name as region, cont.name as continent FROM countries c JOIN regions r ON c.region_id = r.region_id JOIN continents cont ON r.continent_id = cont.continent_id WHERE c.name LIKE ? ORDER BY c.name;";
@@ -39,10 +41,22 @@ public class Main {
                 }
             }
             try (Connection connection = DriverManager.getConnection(url, user, password)) {
-                // BONUS
-                String sqlMilestoneBonus = "SELECT c.name as country_name, GROUP_CONCAT(DISTINCT l.language) as languages, cs.year, cs.population, cs.gdp FROM languages l JOIN country_languages cl ON l.language_id = cl.language_id JOIN countries c ON cl.country_id = c.country_id JOIN country_stats cs ON c.country_id = cs.country_id WHERE c.country_id LIKE ? GROUP BY c.name, cs.year DESC, cs.population, cs.gdp;";
-                System.out.println("What country id would you like to see?");
-                int wantedId = Integer.parseInt(scan.nextLine());
+                // MILESTONE 4 BONUS
+                String sqlMilestoneBonus = null;
+                int wantedId = 0;
+                boolean validID = false;
+                while (!validID) {
+                        sqlMilestoneBonus = "SELECT c.name as country_name, GROUP_CONCAT(DISTINCT l.language) as languages, cs.year, cs.population, cs.gdp FROM languages l JOIN country_languages cl ON l.language_id = cl.language_id JOIN countries c ON cl.country_id = c.country_id JOIN country_stats cs ON c.country_id = cs.country_id WHERE c.country_id LIKE ? GROUP BY c.name, cs.year DESC, cs.population, cs.gdp;";
+                        System.out.println("What country would you like to check?\nPlease, insert a valid ID between 1 and 239");
+                        wantedId = Integer.parseInt(scan.nextLine());
+                    if(wantedId < 1 || wantedId > 239) {
+                        System.out.println("Invalid input, please insert insert a valid ID between 1 and 239 ");
+                        System.out.println(" ");
+
+                    } else {
+                        validID = true;
+                    }
+                }
                 scan.close();
                 try (PreparedStatement ps = connection.prepareStatement(sqlMilestoneBonus)) {
 
